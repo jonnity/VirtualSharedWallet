8db3c1d816348f9f396ec053dd6fb9f79c45d08f
takahashi <takahashi.yuto2021@gmail.com>
2021-07-09 13:26:02 +0900

    userInfoを本格的に作り始めた

--- virtual_shared_wallet/src/components/userInfo.vue
+++ virtual_shared_wallet/src/components/userInfo.vue
@@ -4,16 +4,29 @@
             <p>{{userName}}</p>
         </v-row>
         <v-row id="id_user_payment">
-            <v-collum>
+            <v-column>
                 <p>支払金額：{{userPayedMount}}</p>
-                <p>
-                    <input type="number"><button id="id_button_pay">加算</button>
-                </p>
-            </v-collum>
+                <v-text-field
+                    label="Amount"
+                    value="0"
+                    prefix="￥"
+                ></v-text-field>
+                <div class="my-2">
+                    <v-btn
+                        color="warning"
+                        fab
+                        dark
+                    >
+                        <v-img src="./../assets/お札を受け取る手のフリーアイコン_矢印付き.svg"></v-img>
+                    </v-btn>
+                </div>
+
+                <button id="id_button_pay">加算</button>
+            </v-column>
         </v-row>
-        <v-row id="id_user_">
+        <v-row id="id_user_split_payment">
             <span>調整金額：</span>
-            <span>{{userMountToSplitPay}}</span>
+            <span>{{userSplitPayment}}</span>
         </v-row>
     </v-container>
 </template>
@@ -25,12 +38,17 @@ export default {
         return {
             userName: "割り勘太郎",
             userPayedMount: 0,
-            userMountToSplitPay: 0
+            userPaymentTpSplit: 0
         }
     }
 }
 </script>
 
 <style>
-    .
+    /* #id_user_payment p{
+        margin-bottom: 0%;
+    } */
+    v-row{
+        margin-bottom: 8px;
+    }
 </style>
