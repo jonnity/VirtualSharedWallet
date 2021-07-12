93f8a36c528ef7bd558b79c3d58b2bd5282a1824
takahashi <takahashi.yuto2021@gmail.com>
2021-07-12 17:00:15 +0900

    ベタが輝にしたら名前入力ができなくなっとるなんで

--- virtual_shared_wallet/src/components/users.vue
+++ virtual_shared_wallet/src/components/users.vue
@@ -14,6 +14,7 @@
             <v-col>
                 <v-btn
                     @click="appendUser"
+                    :disabled="userNum>=10"
                     large
                     color="primary"
                     dark
@@ -25,14 +26,23 @@
         <v-row>
             <p>{{ totalPayment }}</p>
         </v-row>
-        <v-row id="id_users">
-            <userInfo id="id_test_test" style="display: table;" @userPayEvent="calcTotalPayment"></userInfo>
+        <v-row>
+            <userInfo id="id_user01" v-if="userNum>=1"  :userName="userNameList[0]" @userPayEvent="calcTotalPayment"></userInfo>
+            <userInfo id="id_user02" v-if="userNum>=2"  :userName="userNameList[1]" @userPayEvent="calcTotalPayment"></userInfo>
+            <userInfo id="id_user03" v-if="userNum>=3"  :userName="userNameList[2]" @userPayEvent="calcTotalPayment"></userInfo>
+            <userInfo id="id_user04" v-if="userNum>=4"  :userName="userNameList[3]" @userPayEvent="calcTotalPayment"></userInfo>
+            <userInfo id="id_user05" v-if="userNum>=5"  :userName="userNameList[4]" @userPayEvent="calcTotalPayment"></userInfo>
+            <userInfo id="id_user06" v-if="userNum>=6"  :userName="userNameList[5]" @userPayEvent="calcTotalPayment"></userInfo>
+            <userInfo id="id_user07" v-if="userNum>=7"  :userName="userNameList[6]" @userPayEvent="calcTotalPayment"></userInfo>
+            <userInfo id="id_user08" v-if="userNum>=8"  :userName="userNameList[7]" @userPayEvent="calcTotalPayment"></userInfo>
+            <userInfo id="id_user09" v-if="userNum>=9"  :userName="userNameList[8]" @userPayEvent="calcTotalPayment"></userInfo>
+            <userInfo id="id_user10" v-if="userNum>=10" :userName="userNameList[9]" @userPayEvent="calcTotalPayment"></userInfo>
         </v-row>
     </v-container>
 </template>
 
 <script>
-import Vue from 'vue';
+// import Vue from 'vue';
 import userInfo from './userInfo.vue';
 
 export default {
@@ -43,41 +53,15 @@ export default {
     data: function(){
         return{
             appendedUserName: "",
-            userNum: 1,
+            userNum: 0,
             totalPayment: 0,
+            userNameList: [],
         }
     },
     methods:{
         appendUser: function(){
-            // const div = document.createElement('div');
-            // const divHTML = '<userInfo id="id_user_' + this.userNum + '" @userPayEvent="calcTotalPayment"></userInfo>';
-            // div.innerHTML = divHTML;
-            // console.log(divHTML)
-            // div.innerHTML = '<userInfo id="id_user_1 @userPayEvent="calcTotalPayment"></userInfo>';
-            // document.getElementById('id_users').appendChild(div);
-            
-            const userComponent = Vue.extend(userInfo);
-            const instance = new userComponent({
-                template: '<userInfo id="id_user_1 @userPayEvent="calcTotalPayment"></userInfo>',
-                propsData: {
-                    userName: this.appendedUserName
-                },
-                parent: this
-            });
-            // instance.$mount("#user_" + this.userNum);
-            instance.$mount();
-            // document.getElementById('id_user_' + this.userNum).appendChild(instance.$el);
-            document.getElementById('id_users').appendChild(instance.$el);
-            // instance.$parent = document.getElementById("id_component_users")
-            console.log(this)
-            console.log(instance.$el)
-            
             this.userNum++;
-            this.appendedUserName = "";
-            // console.log(this.$parent.$el);
-            // console.log(instance);
-            // console.log(document.getElementById('id_component_users'));
-            // console.log(document.getElementById('id_user_1'))
+            this.userNameList.append(this.appendedUserName);
         },
         calcTotalPayment: function(payment_amount){
             this.totalPayment += payment_amount;
