edb8d2d84ff0d6f48ae12d5b4127bd6d70b1f2b2
takahashi <takahashi.yuto2021@gmail.com>
2021-07-15 10:42:40 +0900

    Vue Routerを追加してみた（よくわからないけど、使わずに動くよう）

--- virtual_shared_wallet/src/main.js
+++ virtual_shared_wallet/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import '@mdi/font/css/materialdesignicons.css'
+// import router from './router'
 
 Vue.config.productionTip = false
 
@@ -9,6 +10,8 @@ new Vue({
     icons: {
         iconfont: 'mdi',
     },
+
     vuetify,
+    // router,
     render: h => h(App)
 }).$mount('#app')
