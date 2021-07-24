56d71f5822d1aa03fe18418ce7a6caf9d0b5342e
DESKTOP-PFMOHCA\jonny <jonny1996_ty@yahoo.co.jp>
2021-07-25 00:25:00 +0900

    よくわからんけどhistoryつかったらリロードしても大丈夫になった

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const serveStatic = require('serve-static')
+const history = require('connect-history-api-fallback');
+
 const path = require('path')
+
 const app = express()
-    //we are configuring dist to serve site files
+app.use(history());
+
 app.use('/', serveStatic(path.join(__dirname, '/wa_warikan/dist')))
-    // this * route is to serve project on different page routes except root `/`
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/wa_warikan/dist/index.html'))
 })
