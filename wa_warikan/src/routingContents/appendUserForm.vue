8e53ca92a5a29de7217cefb689f89de134fbd62e
takahashi <takahashi.yuto2021@gmail.com>
2021-07-27 09:47:25 +0900

    まだ微妙にバグ出てる（ユーザー名の重複なくすところがだけど）

--- wa_warikan/src/routingContents/appendUserForm.vue
+++ wa_warikan/src/routingContents/appendUserForm.vue
@@ -37,8 +37,9 @@ export default {
     methods: {
         formatNameAndEmitEvent: function(){
             let appendedUserName = this.inputtedUserName;
+            const sameNameReg = new RegExp("/"+this.inputtedUserName+"_\d/");
             const _this = this;
-            const sameNames = this.userNameList.filter(function(name){return (name === _this.inputtedUserName)});
+            const sameNames = this.userNameList.filter(function(name){return name.match(sameNameReg) === [];});
             const sameNameMenberNum = sameNames.length;
             if(sameNameMenberNum > 0){
                 appendedUserName = this.inputtedUserName + "_" + (sameNameMenberNum+1); 
