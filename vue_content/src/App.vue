41baffa64f588b7445d3ab64817f05c2962b7801
takahashi <takahashi.yuto2021@gmail.com>
2021-07-29 17:05:41 +0900

    フッター足してみた

--- vue_content/src/App.vue
+++ vue_content/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <v-app>
     <userRoot></userRoot>
+    <myFooter></myFooter>
   </v-app>
 </template>
 
 <script>
 import userRoot from "./components/userRoot.vue"
+import myFooter from "./components/footer.vue"
 
 export default {
   name: "App",
   components: {
     userRoot,
+    myFooter,
   },
   data: () => ({
     //
