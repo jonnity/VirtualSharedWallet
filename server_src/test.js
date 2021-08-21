3b9b50c398be9235087bc38d46b5f89da2902abc
DESKTOP-PFMOHCA\jonny <jonny1996_ty@yahoo.co.jp>
2021-08-21 22:46:57 +0900

    no message

--- server_src/test.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", function (req, res) {
-  console.log("api get");
-  res.send({ data: "無事，受け取ったぜ" });
-});
-
-module.exports = router;
