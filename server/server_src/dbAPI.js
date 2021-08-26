def125aa3b39f9dd928ac7b6f097b0c42b3ceb74
takahashi <takahashi.yuto2021@gmail.com>
2021-08-26 13:02:45 +0900

    これでどうよ

--- server/server_src/dbAPI.js
+++ server/server_src/dbAPI.js
@@ -8,7 +8,7 @@ const constants = require("./constants");
 function clientConnect() {
   const postgreInfo = {
     connectionString: process.env.DATABASE_URL,
-    ssl: true,
+    ssl: { rejectUnauthorized: false },
     // user: process.env.POSTGRE_USER,
     // password: process.env.POSTGRE_PASS,
     // host: process.env.POSTGRE_HOST,
