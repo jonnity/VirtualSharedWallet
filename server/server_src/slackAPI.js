4319d243db1578dcdc572359e43102fa7db9aea1
DESKTOP-PFMOHCA\jonny <jonny1996_ty@yahoo.co.jp>
2021-08-28 11:53:07 +0900

    これでいけるらしい？

--- server/server_src/slackAPI.js
+++ server/server_src/slackAPI.js
@@ -18,23 +18,27 @@ router.post("/startSlackSession", async function (req, res) {
   console.log(req.body);
   console.log(req.body.team_domain) + "でセッション名決めればいいか．↓とか";
   console.log("slack_" + req.body.team_domain);
-  const userList = await slackClient.users.list();
-  console.log("slackClient.users.list();" + JSON.stringify(userList));
-  // const result = await web.chat.postMessage({
-  //   text: "Hello world!",
-  // });
-  // console.log(result);
-  // return "success";
-  let data = {
-    response_type: "in_channel",
-    text: "302: Found",
-    attachments: [
-      {
-        image_url: "https://http.cat/302.jpg",
-      },
-    ],
-  };
-  res.json(data);
+  try {
+    const userList = await slackClient.users.list();
+    console.log("slackClient.users.list();" + userList.members);
+    // const result = await web.chat.postMessage({
+    //   text: "Hello world!",
+    // });
+    // console.log(result);
+    // return "success";
+    let data = {
+      response_type: "in_channel",
+      text: "302: Found",
+      attachments: [
+        {
+          image_url: "https://http.cat/302.jpg",
+        },
+      ],
+    };
+    res.json(data);
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
